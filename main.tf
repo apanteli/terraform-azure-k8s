13228bacc938e0bf6b6d64a1079977d1846da99e
Andreas <panteliandreas@gmail.com>
2021-02-18 19:14:15 +0200

    Change namespace of nginx-ingress

--- main.tf
+++ main.tf
@@ -63,18 +63,19 @@ provider "helm" {
 }
 
 resource "helm_release" "ingress_nginx" {
-  name       = "nginx-ingress"
+  name       = "my-ingress-nginx"
 
   repository = "https://kubernetes.github.io/ingress-nginx"
   chart      = "ingress-nginx"
 
-  namespace  = "nginx-ingress"
+  namespace  = "ingress-nginx"
   create_namespace = "true"
 
   set {
     name  = "rbac.create"
     value = "true"
   }
+
   depends_on = [
     local_file.azurek8s
   ]
